0dd4d0c930e898d8ac16d539855aeafe64f0403d
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-18 03:10:31 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       receiver.balance += amount
       @status = 'complete'
     else
-      @status =  'Transaction rejected. Please check your account balance.''
+      @status = 'Transaction rejected. Please check your account balance.'
     end
   end
 end
