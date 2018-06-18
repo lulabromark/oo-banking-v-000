a77b69b925acaae4737f8cdcbc41a1fb6cf50e06
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-18 03:24:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Transfer
   attr_accessor :sender, :receiver, :status, :amount
 
@@ -13,6 +15,7 @@ class Transfer
   end
 
   def execute_transaction
+    binding.pry
     if sender.valid? == false
       @amount = 'Transaction rejected. Please check your account balance.'
       @status = 'rejected'
