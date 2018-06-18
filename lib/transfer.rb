a9346bc566de32a46174aee164fc1dadc624eecf
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-18 03:20:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
     elsif status == 'pending'
       sender.balance -= amount
       receiver.balance += amount
-      @status = 'complete'
+      # @status = 'complete'
     end
   end
 end
