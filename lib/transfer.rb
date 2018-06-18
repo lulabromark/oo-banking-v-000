40606d1a756b0b57cdb8e1a67cda4059af5cf9b1
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-18 03:08:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,8 +13,12 @@ class Transfer
   end
 
   def execute_transaction
-    sender.balance -= amount
-    receiver.balance += amount
-    @status = 'complete'
+    if sender.valid? && status == 'pending'
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = 'complete'
+    else
+      puts "Transaction rejected. Please check your account balance."
+    end
   end
 end
